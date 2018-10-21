8a92619c7734d1879f1c1edae550519a81f977f3
appseed246 <guitar.telecaster2017@gmail.com>
2018-10-21 21:52:02 +0900

    p61 fix currency and times

--- src/main/java/money/Money.java
+++ src/main/java/money/Money.java
@@ -17,11 +17,11 @@ abstract class Money {
     }
 
     static Money dollar(int amount) {
-        return new Dollar(amount);
+        return new Dollar(amount, "USD");
     }
 
     static Money franc(int amount) {
-        return new Franc(amount);
+        return new Franc(amount, "CHF");
     }
 
     abstract Money times(int multiplier);
